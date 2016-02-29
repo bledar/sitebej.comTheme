001daf750aafb649e863cd8ffb8fd539b14108f9
bledar <bledi-1@live.com>
2016-02-29 19:18:08 +0100

    Shtuar hederi view port per mobile

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>SiTeBej - Every thing you want to know</title>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class();?>>
